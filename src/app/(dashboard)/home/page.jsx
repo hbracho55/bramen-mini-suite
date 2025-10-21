6a4d771b7662e9cf6c1188bd189c5df6686911d3
Hector Bracho <hector.bracho@bci.cl>
2025-10-20 23:46:47 -0300

    secong commit

---
+++ src/app/(dashboard)/home/page.jsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h1>Home page2!</h1>
+}
---
+++ src/components/layout/horizontal/FooterContent.jsx
@@ -0,0 +1,54 @@
+'use client'
+
+// Next Imports
+import Link from 'next/link'
+
+// Third-party Imports
+import classnames from 'classnames'
+
+// Hook Imports
+import useHorizontalNav from '@menu/hooks/useHorizontalNav'
+
+// Util Imports
+import { horizontalLayoutClasses } from '@layouts/utils/layoutClasses'
+
+const FooterContent = () => {
+  // Hooks
+  const { isBreakpointReached } = useHorizontalNav()
+
+  return (
+    <div
+      className={classnames(horizontalLayoutClasses.footerContent, 'flex items-center justify-between flex-wrap gap-4')}
+    >
+      <p>
+        <span>{`© ${new Date().getFullYear()}, All rights reserved `}</span>
+        <span>{` by `}</span>
+        <Link href='https://mui.com/store/contributors/themeselection' target='_blank' className='text-primary'>
+          BRAMEN
+        </Link>
+      </p>
+      {!isBreakpointReached && (
+        <div className='flex items-center gap-4'>
+          <Link href='https://mui.com/store/license' target='_blank' className='text-primary'>
+            License
+          </Link>
+          <Link href='https://mui.com/store/contributors/themeselection' target='_blank' className='text-primary'>
+            More Themes
+          </Link>
+          <Link
+            href='https://demos.themeselection.com/marketplace/materio-mui-nextjs-admin-template/documentation'
+            target='_blank'
+            className='text-primary'
+          >
+            Documentation
+          </Link>
+          <Link href='https://themeselection.com/support' target='_blank' className='text-primary'>
+            Support
+          </Link>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default FooterContent
